2b9ca2d1aba5fcbba49792dd41ef0ee95dff1126
AnschiPanchi <anshg1948@gmail.com>
2025-09-20 15:53:09 +0530

    util code stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import {app} from './app.js'
+
+
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen( process.env.PORT||8000,()=>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+        
+    })
+
+    app.get('/instagram',(req,res)=>{
+        res.send("oh welcome to instagram");
+    })
+
+    app.get('/',(req,res)=>{
+        res.send("welcome to home page");
+    })
+})
+.catch((error)=>{
+    console.log("mongodb conection failed.",error);
+    
+})
